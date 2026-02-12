cb3f9b274e6a18d0700b44845fa08a05236b13f8
OPOKA ERIC <opokaeric9@gmail.com>
2026-02-12 11:02:11 +0300

    Installed resend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const response = await resend.emails.send({
-      from: "Family Harvest Foundation <onboarding@resend.dev>", 
+      from: "Family Harvest Foundation <admin@reelmafamilyharvest.org>", 
       to,
       subject,
       html,
